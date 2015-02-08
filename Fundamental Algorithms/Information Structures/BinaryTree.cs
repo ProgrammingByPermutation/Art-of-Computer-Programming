a4a72226e86bdb1fb1cae4adf582b201ecba888f
Justin <cyr.justin@gmail.com>
2015-02-08 18:16:41 -0500

    Adding traversal techniques
    Signed-off-by: Justin <cyr.justin@gmail.com>

--- Fundamental Algorithms/Information Structures/BinaryTree.cs
+++ Fundamental Algorithms/Information Structures/BinaryTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Fundamental_Algorithms.Information_Structures
@@ -29,7 +30,8 @@ namespace Fundamental_Algorithms.Information_Structures
         /// Visit the root
         /// Traverse the right subtree
         /// </summary>
-        /// <param name="action">The action to perform that currently visited node.</param>
+        /// <param name="action">The action to perform that currently visited node. It is assumed that
+        /// action with neither delete the current node nor any of its ancestors from the tree.</param>
         public void TraverseInorder(Action<BinaryTreeNode<T>> action) {
             if (null == action) {
                 return;
@@ -60,6 +62,97 @@ namespace Fundamental_Algorithms.Information_Structures
                 P = A.Pop();
                 action(P);
                 P = P.Right;
+            }
+        }
+
+        /// <summary>
+        /// Performs a preorder traversal of the tree. A preorder traversal is performed
+        /// as follows:
+        /// Visit the root
+        /// Traverse the left subtree
+        /// Traverse the right subtree
+        /// </summary>
+        /// <param name="action">The action to perform that currently visited node. It is assumed that
+        /// action with neither delete the current node nor any of its ancestors from the tree.</param>
+        public void TraversePreorder(Action<BinaryTreeNode<T>> action) {
+            if (null == action) {
+                return;
+            }
+
+            // Let Tree be a pointer to a binary tree; this algorithm visits all nodes of the binary tree preorder,
+            // making use of an auxiliary stack A.
+            
+            // T1. [Initialize] Set stack A empty, and set the link variable P <- Tree
+            Stack<BinaryTreeNode<T>> A = new Stack<BinaryTreeNode<T>>();
+            BinaryTreeNode<T> P = Root;
+
+            while (null != P || A.Count > 0) {
+                // T2. [P = null?] If P = null, go to step T4.
+                while (null != P) {
+                    // MODIFICATION STEP: Visit NODE(P).
+                    // T3. (Now P points to a nonempty binary tree that is to be traversed.) Set A <= P; that is, push
+                    // the value of P onto stack A. Then set P <- LLINK(P) and return to step T2.
+                    action(P);
+                    A.Push(P);
+                    P = P.Left;
+                }
+
+                // T4. [P <= Stack.] If stack A is empty, the algorithm terminates; otherwise set P <= A.
+                if (A.Count <= 0) {
+                    return;
+                }
+
+                // T5. Then set P <- RLINK(P) and return to step T2.
+                P = A.Pop();
+                P = P.Right;
+            }
+        }
+
+        /// <summary>
+        /// Performs a postorder traversal of the tree. A postorder traversal is performed
+        /// as follows:
+        /// Traverse the left subtree
+        /// Traverse the right subtree
+        /// Visit the root
+        /// </summary>
+        /// <param name="action">The action to perform that currently visited node. It is assumed that
+        /// action with neither delete the current node nor any of its ancestors from the tree.</param>
+        public void TraversePostorder(Action<BinaryTreeNode<T>> action) {
+            if (null == action) {
+                return;
+            }
+
+            // Let Tree be a pointer to a binary tree; this algorithm visits all nodes of the binary tree postorder,
+            // making use of an auxiliary stack A.
+            
+            // Create two stacks:
+            // toCheckChildren = The nodes that need to be checked for children
+            // toVisit = The nodes, in order, that need to be visited
+            Stack<BinaryTreeNode<T>> toCheckChildren = new Stack<BinaryTreeNode<T>>();
+            Stack<BinaryTreeNode<T>> toVisit = new Stack<BinaryTreeNode<T>>();
+
+            // Push root onto the stack
+            toCheckChildren.Push(Root);
+
+            // While the stack has content
+            while (toCheckChildren.Count > 0) {
+                BinaryTreeNode<T> node = toCheckChildren.Pop();
+                toVisit.Push(node);
+
+                // Push the left child if it exists
+                if (null != node.Left) {
+                    toCheckChildren.Push(node.Left);
+                }
+
+                // Push the right child if it exists
+                if (null != node.Right) {
+                    toCheckChildren.Push(node.Right);
+                }
+            }
+
+            // While there are nodes to visit call the action delegate
+            while (toVisit.Count > 0) {
+                action(toVisit.Pop());
             }
         }
     }
--- Test Fundamental Algorithms/Information Structures/BinaryTreeUnitTest.cs
+++ Test Fundamental Algorithms/Information Structures/BinaryTreeUnitTest.cs
@@ -27,5 +27,49 @@ namespace Test_Fundamental_Algorithms.Information_Structures
             // in the correct order.
             Assert.AreEqual("DBAEGCHFJ", output);
         }
+
+        [TestMethod]
+        public void PreorderTraversalTestMethod()
+        {
+            // Sample tree: a.b.d, a.c.e.f, a.c.f.h, a.c.f.j
+            BinaryTree<string> binaryTree = new BinaryTree<string>(new BinaryTreeNode<string>("A"));
+            binaryTree.Root.Left = new BinaryTreeNode<string>("B");
+            binaryTree.Root.Left.Left = new BinaryTreeNode<string>("D");
+            binaryTree.Root.Right = new BinaryTreeNode<string>("C");
+            binaryTree.Root.Right.Left = new BinaryTreeNode<string>("E");
+            binaryTree.Root.Right.Right = new BinaryTreeNode<string>("F");
+            binaryTree.Root.Right.Left.Right = new BinaryTreeNode<string>("G");
+            binaryTree.Root.Right.Right.Left = new BinaryTreeNode<string>("H");
+            binaryTree.Root.Right.Right.Right = new BinaryTreeNode<string>("J");
+
+            string output = string.Empty;
+            binaryTree.TraversePreorder(node => output += node.Value);
+
+            // Preorder Traversal test should output "ABDCEGFHJ" if nodes are being visited
+            // in the correct order.
+            Assert.AreEqual("ABDCEGFHJ", output);
+        }
+
+        [TestMethod]
+        public void PostorderTraversalTestMethod()
+        {
+            // Sample tree: a.b.d, a.c.e.f, a.c.f.h, a.c.f.j
+            BinaryTree<string> binaryTree = new BinaryTree<string>(new BinaryTreeNode<string>("A"));
+            binaryTree.Root.Left = new BinaryTreeNode<string>("B");
+            binaryTree.Root.Left.Left = new BinaryTreeNode<string>("D");
+            binaryTree.Root.Right = new BinaryTreeNode<string>("C");
+            binaryTree.Root.Right.Left = new BinaryTreeNode<string>("E");
+            binaryTree.Root.Right.Right = new BinaryTreeNode<string>("F");
+            binaryTree.Root.Right.Left.Right = new BinaryTreeNode<string>("G");
+            binaryTree.Root.Right.Right.Left = new BinaryTreeNode<string>("H");
+            binaryTree.Root.Right.Right.Right = new BinaryTreeNode<string>("J");
+
+            string output = string.Empty;
+            binaryTree.TraversePostorder(node => output += node.Value);
+
+            // Postorder Traversal test should output "DBGEHJFCA" if nodes are being visited
+            // in the correct order.
+            Assert.AreEqual("DBGEHJFCA", output);
+        }
     }
 }
